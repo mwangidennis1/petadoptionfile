77fc9b83e1f71473bfca5bf3b666937611d9164d
p2X0 <mwangedennis05@gmail.com>
2024-03-14 12:54:28 +0300

    add seed for persistence

--- animals/urls.py
+++ animals/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import AnimalListView,AnimalDetailView
+from .views import AnimalListView,AnimalDetailView,AnimalAdoptView
 
 urlpatterns=[
     path('',AnimalListView.as_view(),name='animal_list'),
-    path('<uuid:pk>',AnimalDetailView.as_view(),name='animal_detail')
+    path('<uuid:pk>',AnimalDetailView.as_view(),name='animal_detail'),
+    path('adopt/',AnimalAdoptView.as_view(),name='animal_adopt')
 ]
